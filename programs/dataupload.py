162fd6500f6153c30c5220b52c8327605f8b5ad8
vincecedo <151643069+vincecedo@users.noreply.github.com>
2023-11-27 13:54:01 +0100

    Update dataupload.py

--- programs/dataupload.py
+++ programs/dataupload.py
@@ -1,1 +1,37 @@
+import evdev
+
+def communicate_with_scanner():
+    # Find the barcode scanner device
+    devices = [evdev.InputDevice(fn) for fn in evdev.list_devices()]
+    scanner_device = None
+
+    for device in devices:
+        if "barcode" in device.name.lower():
+            scanner_device = device
+            break
+
+    if scanner_device is None:
+        raise ValueError("Barcode scanner not found.")
+
+    print("Found barcode scanner:", scanner_device)
+
+    # Read input events from the barcode scanner
+    scanner_data = []
+    for event in scanner_device.read_loop():
+        if event.type == evdev.ecodes.EV_KEY and event.value == 1:
+            # Assuming barcode data is sent as key events when a key is pressed
+            key_code = evdev.ecodes.KEY[event.code]
+
+            if key_code == 'KEY_ENTER':
+                process_scanner_data(''.join(scanner_data))
+                scanner_data = []
+            else:
+                scanner_data.append(key_code)
+
+def process_scanner_data(data):
+    # Implement logic to process the barcode data received from the scanner
+    print("Processing scanner data:", data)
+
+if __name__ == "__main__":
+    communicate_with_scanner()
 
